4792e4712519fc724a3c71d61191a11d8a9ffa84
tanjiro <madhanking2004ac@gmail.com>
2025-09-14 11:46:24 +0530

    V1.0 CODE:8 Production Published

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 28
         targetSdk = 35
-        versionCode = 6
+        versionCode = 8
         versionName = "1.9"
     }
 
